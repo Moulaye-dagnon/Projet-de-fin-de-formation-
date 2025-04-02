98bfcf2ad14068570e371233d32d63451d74fe91
Cheickna01 <diabyhamala0@gmail.com>
2025-04-02 20:45:24 +0100

    intégrer la photo de profile

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const cors = require("cors")
 const dotenv = require("dotenv").config
 const router = require("./routes/userRoutes")
+const multer = require("multer");
+const path = require("path");
 const app = express();
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use("/",router)
 
 
--- server/models/user.js
+++ server/models/user.js
@@ -8,7 +8,7 @@ const userSchema = new Schema({
     },
     prenom: {
         type: String,
-        require: true
+        require: true 
     },
     username: {
         type: String,
@@ -30,6 +30,9 @@ const userSchema = new Schema({
         type: String,
         default: "collaborateur",
         require: true
+    },
+    photoProfil: {
+        type: String
     },
     projets: [{
         projet: {
