3387199cea0b9c5041be96429a24b62bb530492e
Cheickna01 <diabyhamala0@gmail.com>
2025-04-10 05:10:05 +0100

    tester l'authentification

--- client/src/Components/Dashboard.jsx
+++ client/src/Components/Dashboard.jsx
@@ -1,10 +1,29 @@
 import { useContext, useEffect } from "react"
 import { UserContext } from "./UserContext"
+import { useNavigate } from "react-router-dom"
 
 export default function Dashboard() {
+  const navigate = useNavigate()
   const {user,token,logout} = useContext(UserContext)
-  console.log(user)
   console.log(token)
+  useEffect(()=>{
+    fetch("http://localhost:4000/",{
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
+    }).then(req=>{
+      if(req.status === 401){
+        logout()
+        navigate("/login")
+      }else{
+        return req.json()
+      }
+    }).then(res=> {
+      console.log(res)
+    })
+  },[user,token,logout])
   return (
     <div>
       {token && <p>token récupérer</p>}
--- client/src/Components/UserContext.jsx
+++ client/src/Components/UserContext.jsx
@@ -6,10 +6,11 @@ export function UserProvider({ children }) {
   const [user, setUser] = useState(() => {
     return JSON.parse(localStorage.getItem("user"));
   });
-
+  
   const [token, setToken] = useState(() => {
     return localStorage.getItem("token");
   });
+  console.log(localStorage.getItem("token"))
 
   function login(user,token){
     setUser(user)
