926a4faad567784b72eff6d04a9cc3867b3a5fa2
Cheickna01 <diabyhamala0@gmail.com>
2025-06-13 05:55:03 +0100

    change api links

--- server/index.js
+++ server/index.js
@@ -64,10 +64,10 @@ app.use("/api", router);
 app.use("/api", ProjectRouter);
 app.use("/api", TaskRouter);
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
 
