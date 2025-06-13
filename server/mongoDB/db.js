d85fdbfb4112c075167e7833dc70299dbd161057
Cheickna01 <147637808+Cheickna01@users.noreply.github.com>
2025-06-13 01:28:05 +0100

    Update db.js

--- server/mongoDB/db.js
+++ server/mongoDB/db.js
@@ -4,7 +4,6 @@ const dotenv = require("dotenv").config();
 const connection = () => {
   try {
     mongoose.connect(process.env.MONGO_URI);
-    console.log(process.env.MONGO_URI)
     console.log("Connection à la base de données reussie!!!");
   } catch (error) {
     console.log("erreur lors de la connexion à la base de donnée...");
